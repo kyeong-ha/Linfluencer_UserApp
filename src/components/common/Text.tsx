6f6586fe9c120cababea45bb44ff6ae9e7aaa755
KyeongHa <rudgk030@hanyang.ac.kr>
2023-09-14 16:00:08 +0900

    Convert .js(x) to .ts(x)

--- src/components/common/Text.tsx
+++ src/components/common/Text.tsx
@@ -1,18 +1,25 @@
-// import { ReactNode } from 'react';
-// import styled from "styled-components";
+import { ReactNode } from 'react';
+import styled from "styled-components";
 
-// const Txt = styled.p`
-//   font-size: ${(size) => (size ? `${size}` : '1rem')};
-//   font-weight: ${(weight) => (weight ? `${weight}` : 'normal')};
-//   white-space: pre-line;
-// `;
+const Txt = styled.p`
+  font-size: ${(size) => (size ? `${size}` : '1rem')};
+  font-weight: ${(weight) => (weight ? `${weight}` : 'normal')};
+  white-space: pre-line;
+`;
 
-// export default function Text({ children, size, weight, id } : { children: ReactNode[], size: string, weight: string, id: string }) {
-//   return (
-//     <>
-//       <Txt size={size} weight={weight} id={id || ''}>
-//         { children }
-//       </Txt>;
-//     </>
-//   )
-// };
+
+interface TextProps extends HTMLLabelElement{
+  readonly id?: string;
+  readonly size?: string;
+  readonly weight?: string;
+}
+
+export default function Text({ children } : { children: ReactNode[] }, { id, size, weight }: TextProps) {
+  return (
+    <>
+      <Txt size={size} weight={weight} id={id || ''}>
+        { children }
+      </Txt>;
+    </>
+  )
+};

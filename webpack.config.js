6b6eec56100b4410671cb8f1a2743acf241f6590
KyeongHa <rudgk030@hanyang.ac.kr>
2023-09-14 19:23:08 +0900

    Add gh-pages for deploy

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
         ],
     },
     output: {
-        path: path.join(__dirname, "/dist"),
+        path: path.join(__dirname, "/build"),
         filename: "bundle.js",
     },
     plugins: [
